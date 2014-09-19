3f2687a506c6592dca3678be1bfc5a0437e44f36
ccwq <scwqd@qq.com>
2014-09-19 16:06:18 +0800

    cayout 及c_layout.使用js对html以及css的布局功能进行增强

---
+++ jqplug/cayout.js
@@ -0,0 +1,30 @@
+/**
+ * Created with WebStorm.
+ * User: Administrator
+ * Date: 14-9-19
+ * Time: 下午3:40
+ * To change this template use File | Settings | File Templates.
+ */
+define(function (require, exports, module) {
+    /**
+     * 对使用js对html以及css的布局功能进行增强
+     * @type {{fresh: fresh}}
+     */
+    var cayout = {
+
+        init:function(){
+
+        },
+
+        //解析dom片段，增加监控html
+        parse:function(){
+
+        },
+
+        //手动刷新
+        fresh:function(){
+
+        }
+    };
+    return {};
+});

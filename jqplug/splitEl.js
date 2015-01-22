96af36c432fd080e31c6cc38e8c8198e2d2e928a
Dave C <scwqd@qq.com>
2015-01-22 12:59:48 +0800

    onelshow增加缓存提高性能，并用第二参数标识元素是否第一次被展示

--- jqplug/splitEl.js
+++ jqplug/splitEl.js
@@ -32,7 +32,7 @@ define(function (require, exports, module) {
             autoSkipTo:0,
 
             //当翻页(自动或者手动)时候执行，并返回当前被展示的元素
-            //function($els)
+            //function($els,isFirstShowing) //参数1将要被显示的元素,参数2如果为true表示是第一次被展示（一般用户对元素内部功能进行初始化）
             onelshow: $.noop,
 
 
@@ -41,23 +41,30 @@ define(function (require, exports, module) {
             row:9,                      //每页显示多少，默认9,
             autoSkipTo:1,               //默认跳转到页，如果小于1不跳转,
             skipFunc:function($list,pageno, row){
+                var m = this;
                 $list.hide().removeClass("showing");
-                var cur = 0;
-                var willShowEls = $();
-                $list.each(function(i){
-                    var t= $(this);
-                    var pnofix = pageno - 1;
-                    if(i >= pnofix * row && i < (pnofix + 1) * row){
-                        willShowEls[willShowEls.length++] = this;
 
-                    }
-                    cur ++;
-                });
+                var cacheid = $list.length + "_" + pageno +"_" + "row";
+                var willShowEls = m.skipfuc_cache[cacheid];
+                var isNewCreated = false;
+                if(!willShowEls){
+                    willShowEls = $();
+                    isNewCreated = true;
+                    var cur = 0;
+                    $list.each(function(i){
+                        var t= $(this);
+                        var pnofix = pageno - 1;
+                        if(i >= pnofix * row && i < (pnofix + 1) * row){
+                            willShowEls[willShowEls.length++] = this;
+                        }
+                        cur ++;
+                    });
+                    m.skipfuc_cache[cacheid] = willShowEls;
+                }
 
                 willShowEls.show();
-
                 var sett = this.sett;
-                sett.onelshow.call(this,willShowEls);
+                sett.onelshow.call(this,willShowEls,isNewCreated);
 
                 //加载图片
                 willShowEls.find("img[_src]:not(.scrollEle img)").unblockImg()
@@ -70,6 +77,9 @@ define(function (require, exports, module) {
 
         MkListToPage = function($list,config){
             var me = this;
+
+            //用来缓存分页数据
+            me.skipfuc_cache = {};
             var sett = me.sett = $.extend({},def,config);
             $list.parent().show();
             sett.pages = ~~(($list.length-1)/sett.row) + 1;

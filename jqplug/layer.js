925f7e783109b319125c6a26bdad6b587fc1c5f8
ccwq <daveche@trgis.com>
2015-04-24 11:14:50 +0800

    增加 layer控件

---
+++ jqplug/layer.js
@@ -0,0 +1,11 @@
+﻿/**
+* 鉴于layer不能改变和内部资源的相对未知，故使用加载器引用
+* */
+
+define(function(require){
+	var layer = require("./layer/layer");
+	layer.config({
+	    path: require.resolve("./") + "layer/"
+	});
+    return layer;
+});

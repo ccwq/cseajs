3136ef892d248f0d59b6e05cc5d70d9bdf7a679c
ccwq <daveche@trgis.com>
2015-10-17 13:42:44 +0800

    增减观察者模式的事件中心

---
+++ little-event-center.js
@@ -0,0 +1,110 @@
+/**
+ * home
+ * https://github.com/wangxiao/little-event-center
+ * 事件中心
+ * little-event-center.js
+ * @returns {{on: Function, once: Function, emit: Function, off: Function}}
+ */
+var eventCenter = function() {
+    var eventList = {};
+    var eventOnceList = {};
+
+    var _on = function(eventName, fun, isOnce) {
+        if (!eventName) {
+            throw('No event name.');
+        }
+        else if (!fun) {
+            throw('No callback function.');
+        }
+        var list = eventName.split(/\s+/);
+        var tempList;
+        if (!isOnce) {
+            tempList = eventList;
+        } 
+        else {
+            tempList = eventOnceList;
+        }
+        for (var i = 0, l = list.length; i < l; i ++) {
+            if (list[i]) {
+                if (!tempList[list[i]]) {
+                    tempList[list[i]] = [];
+                }
+                tempList[list[i]].push(fun);
+            }
+        }
+    };
+    
+    var _off = function(eventName, fun, isOnce) {
+        var tempList;
+        if (!isOnce) {
+            tempList = eventList;
+        } else {
+            tempList = eventOnceList;
+        }
+        if (tempList[eventName]) {
+            var i = 0;
+            var l = tempList[eventName].length;
+            for (; i < l; i ++) {
+                if (tempList[eventName][i] === fun) {
+                    tempList[eventName].splice(i, 1);
+                    // 每次只清除掉一个
+                    return;
+                }
+            }
+        }
+    };
+
+    return {
+        on: function(eventName, fun) {
+            _on(eventName, fun);
+            return this;
+        },
+        once: function(eventName, fun) {
+            _on(eventName, fun, true);
+            return this;
+        },
+        emit: function(eventName, data) {
+            if (!eventName) {
+                throw('No emit event name.');
+            }
+            var i = 0;
+            var l = 0;
+            var listeners;
+            if (eventList[eventName]) {
+                // 有可能执行过程中，通过 off 删除了某个事件对应的方法，这里先复制一份出来
+                listeners = eventList[eventName].slice();
+                i = 0;
+                l = listeners.length;
+                for (; i < l; i ++) {
+                    listeners[i].call(this, data);
+                }
+            }
+            if (eventOnceList[eventName]) {
+                // 有可能执行过程中，通过 off 删除了某个事件对应的方法，这里先复制一份出来
+                listeners = eventOnceList[eventName].slice();
+                i = 0;
+                l = listeners.length;
+                for (; i < l; i ++) {
+                    listeners[i].call(this, data);
+                    _off(eventName, listeners[i], true);
+                }
+            }
+            return this;
+        },
+        off: function(eventName, fun) {
+            _off(eventName, fun);
+            return this;
+        }
+    };
+};
+
+
+if(typeof define !== "undefined"){
+    if(define.cmd){
+        define(function(){
+            var ec = new eventCenter();
+            ec._class = eventCenter;
+            return ec;
+        })
+    }
+}
